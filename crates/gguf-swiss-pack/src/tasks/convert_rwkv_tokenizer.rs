e3a9fc5a993a1f327311c4901612de440fc41a36
Layl Bongers <3094382+LaylBongers@users.noreply.github.com>
2024-04-12 16:27:53 +0200

    Add token types and correct for mismatch

--- crates/gguf-swiss-pack/src/tasks/convert_rwkv_tokenizer.rs
+++ crates/gguf-swiss-pack/src/tasks/convert_rwkv_tokenizer.rs
@@ -27,16 +27,35 @@ impl PackTask for ConvertRwkvTokenizerTask {
         let vocab_file_path = ctx.source_root().join(&self.manifest.source);
         let vocab_raw =
             std::fs::read_to_string(&vocab_file_path).context("failed to open vocab")?;
-        let vocab = parse_vocab(&vocab_raw)?;
+        let mut vocab = parse_vocab(&vocab_raw)?;
+
+        // Generate a token type map, to tell llama.cpp which ones need to actually be matched in
+        // which ways. Defaulting to 1, which is "normal".
+        let mut token_type = vec![1u32; vocab.len()];
+
+        // Overwrite the type of token 0 for RWKV as 3 (control token)
+        token_type[0] = 3;
+
+        // Extend tokens if necessary, models can provide additional placeholder tokens, mark these
+        // as 5 (unused)
+        let remainder = self.manifest.token_count as i32 - vocab.len() as i32;
+        if remainder < 0 {
+            bail!("\"token_count\" less than provided in the vocab");
+        }
+        for i in 0..remainder {
+            // Every token has to be unique for llama.cpp
+            vocab.push(format!("<unused {}>", i).into_bytes());
+            token_type.push(5);
+        }
 
         // Insert tokenizer into metadata
         ctx.push_metadata_str("tokenizer.ggml.model", "rwkv");
-        ctx.push_metadata_value(
-            "tokenizer.ggml.tokens",
-            MetadataValue::Array(MetadataArray::String(vocab)),
-        );
 
-        // TODO: Correct for token count mismatch, some models have additional placeholder tokens
+        let vocab_value = MetadataValue::Array(MetadataArray::String(vocab));
+        ctx.push_metadata_value("tokenizer.ggml.tokens", vocab_value);
+
+        let vocab_value = MetadataValue::Array(MetadataArray::UInt32(token_type));
+        ctx.push_metadata_value("tokenizer.ggml.token_type", vocab_value);
 
         Ok(())
     }
@@ -46,7 +65,6 @@ impl PackTask for ConvertRwkvTokenizerTask {
 struct ConvertRwkvTokenizerManifest {
     pub source: String,
 
-    #[allow(dead_code)]
     pub token_count: u64,
 }
 
